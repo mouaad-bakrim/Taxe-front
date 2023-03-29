c85ad2ae1c16276fe2897680bab5489963e01f98
Azdoud1 <azdoudmohamed1999@gmail.com>
2023-03-29 13:22:40 +0200

    notification

--- src/app/view/notification-local/notification-local-list/notification-local-list.component.ts
+++ src/app/view/notification-local/notification-local-list/notification-local-list.component.ts
@@ -1,50 +1,57 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {NotificationLocalService} from "../../../controller/service/notification-local.service";
 import {NotificationLocal} from "../../../controller/model/notification-local.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-notification-local-list',
   templateUrl: './notification-local-list.component.html',
   styleUrls: ['./notification-local-list.component.css']
 })
-export class NotificationLocalListComponent implements OnInit{
-
-
-  constructor(private _notificationLocalService: NotificationLocalService) {}
-
-  get notificationLocalService(): NotificationLocalService {
-    return this._notificationLocalService;
-  }
-
-  set notificationLocalService(value: NotificationLocalService) {
-    this._notificationLocalService = value;
-  }
-
-  public findAll():void {
-    this.notificationLocalService.findAll().subscribe(data => {
-        this.notifications= data
-      }, error => {
-        alert('Error');
-      }
-    );
-  }
-  get notification(): NotificationLocal {
-    return this._notificationLocalService.notification;
-  }
-
-  set notification(value: NotificationLocal) {
-    this._notificationLocalService.notification = value;
-  }
-
-  get notifications(): Array<NotificationLocal> {
-    return this._notificationLocalService.notifications;
-  }
-
-  set notifications(value: Array<NotificationLocal>) {
-    this._notificationLocalService.notifications = value;
-  }
+export class NotificationLocalListComponent{
 
   ngOnInit(): void {
     this.findAll();
   }
+  constructor(private notificationLocalService: NotificationLocalService) {}
+
+
+
+  public findAll():void {
+    this.notificationLocalService.findAll().subscribe(data => {
+        this.notifications= data;});
+  }
+
+  public deleteById(notification:NotificationLocal,  index: number): void {
+    this.notification=notification;
+    this.notificationLocalService.deleteById(notification.id).subscribe(data => {
+      if (data>0){
+        this.notifications.splice(index,1);
+      }else {
+        alert('DEL ERROR')
+      }
+    });
+  }
+
+
+
+
+
+
+  get notification(): NotificationLocal {
+    return this.notificationLocalService.notification;
+  }
+
+  set notification(value: NotificationLocal) {
+    this.notificationLocalService.notification = value;
+  }
+
+  get notifications(): Array<NotificationLocal> {
+    return this.notificationLocalService.notifications;
+  }
+
+  set notifications(value: Array<NotificationLocal>) {
+    this.notificationLocalService.notifications = value;
+  }
+
 }
