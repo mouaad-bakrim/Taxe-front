724abe82d7d17dfb660820a3991cf6ea7c123eac
ismail aouifia <aouifiaismail@gmail.com>
2023-03-29 09:57:05 +0000

    commit

--- src/app/controller/model/taux-taxe-anuelle.model.ts
+++ src/app/controller/model/taux-taxe-anuelle.model.ts
@@ -2,13 +2,12 @@ import {CategorieLocal} from "./categorie-local.model";
 
 
 export class TauxTaxeAnuelle {
-  public id!:number;
-  public pourcentageRetard!:number;
-  public pourcentageMajoration!: number;
+  public id!: number;
+  public pourcentageRetardAnnuelle!: number;
+  public pourcentageMajorationAnnuelle!: number;
 
-  public dateApplicationFin!:Date;
-  public dateApplicationDebut!:Date;
-
+  public dateApplicationFin!: Date;
+  public dateApplicationDebut!: Date;
 
 
   public categorielocale = new CategorieLocal();
